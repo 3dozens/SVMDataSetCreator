e9a88ab5c8d9cef0db8ea0592509fb7fb3e97747
kento_hayasaka <thunders.1028@gmail.com>
2016-02-03 16:05:26 +0900

    modify: crawl article urls process move to URLProcessor

--- SVMDataSetFromHTML.java
+++ SVMDataSetFromHTML.java
@@ -5,55 +5,30 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 public class SVMDataSetFromHTML {
 	
 	private static final String FILENAME = "/Users/thunders/research/assignments/SVM_Light/myOwnProduct/DataSet.txt";
 	private static final int ARTICLE_NUMBER_PER_CATEGORY = 500;
+	private static final int CRAWL_PAGE_NUMBER_PER_CATEGORY = ARTICLE_NUMBER_PER_CATEGORY / 25; // クロールするページ数
 //	private static final int CATEGORY_NUM = 2;
   
+    /**
+     * Yahooニュースの記事からSVMLightのデータセットの形式で
+     * FeatureVector値をファイルに出力します
+     */
     public final static void main(String[] args) throws Exception {
 
 		List<String> articleURLs = new ArrayList<>();
 
 		// 経済ニュースのURLを取得する
-		// 1ループあたり"約"25件の記事URLを取得する。なぜか件数が1〜2件ズレる。境界値の問題かもしれない
-		int economyArticleNum = 0; // 件数がズレてしまうため、定数の記事数を使わずにsize()で実際の件数を数える
-		for (int i = 1; i <= ARTICLE_NUMBER_PER_CATEGORY / 25; i++) {
-			
-			// Yahooニュースの経済トップページを取得
-			Document economyTopPage = Jsoup.connect("http://news.yahoo.co.jp/hl")
-					.data("c", "bus_all")
-					.data("p", String.valueOf(i))
-					.get();
-
-			// 新着記事リストから記事URLを抽出
-			Element articleListUl = economyTopPage.getElementsByClass("listBd").get(0);
-			Elements Atags = articleListUl.getElementsByTag("a");
-			articleURLs.addAll(Atags.stream().map(a -> a.attr("href")).collect(Collectors.toList()));
-			economyArticleNum = articleURLs.size(); // 件数がズレてしまうため、定数の記事数を使わずにsize()で実際の件数を数える
-		}
+		List<String> economyArticleURLs = URLProcessor.crawlArticleURLs("bus_all", CRAWL_PAGE_NUMBER_PER_CATEGORY);
+		int economyArticleNum = economyArticleURLs.size(); // 件数がズレてしまうため、定数の記事数を使わずにsize()で実際の件数を数える
+		articleURLs.addAll(economyArticleURLs);
 
 		// スポーツニュースのURLを取得する
-		// 1ループあたり"約"25件の記事URLを取得する
-		for (int i = 1; i <= ARTICLE_NUMBER_PER_CATEGORY / 25; i++) {
-			// Yahooニュースのスポーツトップページを取得
-			Document sportsTopPage = Jsoup.connect("http://news.yahoo.co.jp/hl")
-					.data("c", "spo")
-					.data("p", String.valueOf(i))
-					.get();
-
-			// 新着記事リストから記事URLを抽出
-			Element articleListUl = sportsTopPage.getElementsByClass("listBd").get(0);
-			Elements Atags = articleListUl.getElementsByTag("a");
-			articleURLs.addAll(Atags.stream().map(a -> a.attr("href")).collect(Collectors.toList()));
-		}
+		List<String> sportsArticleURLs = URLProcessor.crawlArticleURLs("spo", CRAWL_PAGE_NUMBER_PER_CATEGORY);
+		articleURLs.addAll(sportsArticleURLs);
 
 		List<String> docs = URLProcessor.convertURLsToDocs(articleURLs);
 
@@ -68,5 +43,4 @@ public class SVMDataSetFromHTML {
 		pw.close();
 	
     }
-    
 }
--- URLProcessor.java
+++ URLProcessor.java
@@ -1,9 +1,12 @@
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class URLProcessor {
 
@@ -28,7 +31,37 @@ public class URLProcessor {
 			System.out.println(i); // 現在何件目を処理中か表示
 			i++;
 		}
+		System.out.println("crawl done.");
 		
 		return documents;
 	}
+	
+	/**
+	 * Yahooニュースのカテゴリートップページの記事一覧から
+	 * そのカテゴリーのニュース記事のURLリストを取得します
+	 * @param category カテゴリーURL
+	 * @param pageNumber 記事一覧の何ページ分の記事URLを取得するか。1ページあたり25件取得します。
+	 * @return ニュース記事URLリスト
+	 * @throws IOException 例外
+	 */
+	public static List<String> crawlArticleURLs(String category, int pageNumber) throws IOException {
+    	List<String> articleURLs = new ArrayList<>();
+    	
+    	// 1ループあたり"約"25件の記事URLを取得する。なぜか件数が1〜2件ズレる。境界値の問題かもしれない
+    	for (int i = 1; i <= pageNumber; i++) {
+			
+			// Yahooニュースのカテゴリートップページを取得
+			Document topPage = Jsoup.connect("http://news.yahoo.co.jp/hl")
+					.data("c", category)
+					.data("p", String.valueOf(i))
+					.get();
+
+			// 新着記事リストから記事URLを抽出
+			Element articleListUl = topPage.getElementsByClass("listBd").get(0);
+			Elements Atags = articleListUl.getElementsByTag("a");
+			articleURLs.addAll(Atags.stream().map(a -> a.attr("href")).collect(Collectors.toList()));
+		}
+    	
+    	return articleURLs;
+    }
 }
